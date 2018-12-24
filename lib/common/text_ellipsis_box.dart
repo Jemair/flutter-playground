a4fb574a57627b1fed2940afaaf67acb06648da6
J <wang940110@163.com>
2018-12-25 00:15:29 +0800

    <feat> 展开折叠

--- lib/common/text_ellipsis_box.dart
+++ lib/common/text_ellipsis_box.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/theme.dart';
 
 class TextEllipsisBoxState extends StatefulWidget {
   TextEllipsisBoxState(this.text, {@required this.maxLine, this.textStyle});
@@ -13,16 +15,43 @@ class TextEllipsisBoxState extends StatefulWidget {
 
 class _TextEllipsisBoxStateState extends State<TextEllipsisBoxState> {
   var isFolded = true;
+  final TapGestureRecognizer recognizer = TapGestureRecognizer();
 
-  @override
-  Widget build(BuildContext context) {
+  _buildExpand() {
+    print(isFolded);
+    if (!isFolded) return Text('');
     return GestureDetector(
       onTap: () {
         this.setState(() {
           isFolded = !isFolded;
         });
       },
-      child: Text(widget.text, style: widget.textStyle, maxLines: isFolded ? widget.maxLine : null),
+      child: Text(
+        '展开',
+        style: TextStyle(color: AppTheme.fontColorLink),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    recognizer.onTap = () {
+      this.setState(() {
+        this.isFolded = true;
+      });
+    };
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: RichText(
+            text: TextSpan(text: widget.text, style: TextStyle(color: Colors.black), children: <TextSpan>[
+              TextSpan(text: ' 折叠', style: TextStyle(color: AppTheme.fontColorLink), recognizer: recognizer,)
+            ]),
+            maxLines: isFolded ? widget.maxLine : null,
+          ),
+        ),
+        _buildExpand(),
+      ],
     );
   }
 }
